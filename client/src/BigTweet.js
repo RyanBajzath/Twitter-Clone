257b9b6c09f58cd29d0b1f293ea8c155fde31cb7
Ryan Bajzath <R.bajzath@hotmail.com>
2022-07-16 22:17:43 -0400

    updated big tweet

--- client/src/BigTweet.js
+++ client/src/BigTweet.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import moment from "moment";
 import { FiHeart, FiMessageCircle, FiDownload, FiRepeat } from "react-icons/fi";
 import styled from "styled-components";
@@ -6,6 +6,33 @@ import { useHistory } from "react-router-dom";
 
 const BigTweet = ({ tweet }) => {
   // console.log(tweet);
+  const [_isLiked, setIsLiked] = useState(0);
+  const [_numLikes, setNumLikes] = useState(0);
+
+  const [_isRetweeted, setIsRetweeted] = useState(0);
+  const [_numRetweets, setNumRetweets] = useState(0);
+
+  const handleLike = (e) => {
+    e.preventDefault();
+    setIsLiked(!_isLiked);
+    if (_isLiked) {
+      setNumLikes(_numLikes - 1);
+    } else {
+      setNumLikes(_numLikes + 1);
+    }
+    // console.log(_numLikes);
+  };
+
+  const handleRetweet = (e) => {
+    e.preventDefault();
+    setIsRetweeted(!_isRetweeted);
+    if (_isRetweeted) {
+      setNumRetweets(_numRetweets - 1);
+    } else {
+      setNumRetweets(_numRetweets + 1);
+    }
+    // console.log(_numRetweets);
+  };
 
   const date = moment(tweet?.timestamp).format("MMM Do ");
 
@@ -21,20 +48,32 @@ const BigTweet = ({ tweet }) => {
           <Photo src={tweet.media[0]?.url} alt="img" />
         )}
         <StyledDate>{date}</StyledDate>
-        <div>
-          <button>
-            <FiHeart />
-          </button>
-          <button>
+        <ButtonWrapper>
+          <StyledButton>
+            <FiHeart
+              //on click to change state
+              onClick={(e) => {
+                handleLike(e);
+              }}
+            />
+
+            <span>{_numLikes}</span>
+          </StyledButton>
+          <StyledButton>
             <FiMessageCircle />
-          </button>
-          <button>
+          </StyledButton>
+          <StyledButton>
             <FiDownload />
-          </button>
-          <button>
-            <FiRepeat />
-          </button>
-        </div>
+          </StyledButton>
+          <StyledButton>
+            <FiRepeat
+              onClick={(e) => {
+                handleRetweet(e);
+              }}
+            />
+            <span>{_numRetweets}</span>
+          </StyledButton>
+        </ButtonWrapper>
       </Wrapper>
     </>
   );
@@ -64,5 +103,15 @@ const SytledStatus = styled.p`
 
 const StyledDate = styled.p`
   margin-left: 200px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+
+  justify-content: space-around;
+`;
+
+const StyledButton = styled.button`
+  border: none;
 `;
 export default BigTweet;
