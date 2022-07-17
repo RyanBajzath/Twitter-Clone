215a305ee0270d67ead1d989aa10ca6c3e9a4af6
Ryan Bajzath <R.bajzath@hotmail.com>
2022-07-17 11:41:48 -0400

    more updates

--- client/src/Error.js
+++ client/src/Error.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { FaBomb } from "react-icons/fa";
+import styled from "styled-components";
 
 const Error = () => {
   return (
-    <div>
+    <Wrapper>
       <FaBomb />
       <h1>An unknown error has occured</h1>
       {/* <span>Please try refreshing the page, or </span> */}
@@ -11,8 +12,17 @@ const Error = () => {
         Please try refreshing the page, or
         <a href="#"> contact support </a> , if the problem continues.
       </span>
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  /* margin-top: 100px; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  /* margin-top: 100px; */
+`;
 
 export default Error;
